9645bb0c2ab703ca6b20e4c58e737096dcc30d2d
sautsokreach <sautsokreach@gmail.com>
2024-03-15 08:24:01 +0700

    15032024823

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -7,6 +7,7 @@ import MoonIcon from '@heroicons/react/24/outline/MoonIcon'
 import SunIcon from '@heroicons/react/24/outline/SunIcon'
 import { openRightDrawer } from '../features/common/rightDrawerSlice';
 import { RIGHT_DRAWER_TYPES } from '../utils/globalConstantUtil'
+import image from '../image/default_profile.png'
 
 import { NavLink,  Routes, Link , useLocation} from 'react-router-dom'
 
@@ -91,7 +92,7 @@ function Header(){
                 <div className="dropdown dropdown-end ml-4">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                        <img src="https://placeimg.com/80/80/people" alt="profile" />
+                        <img src={image} alt="profile" />
                         </div>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
