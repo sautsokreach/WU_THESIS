54e9147ddaea48c2b53e9d75a94d54274d32f3f4
sautsokreach <sautsokreach@gmail.com>
2024-04-03 12:06:14 +0700

    tetst

--- src/utils/globalConstantUtil.js
+++ src/utils/globalConstantUtil.js
@@ -15,7 +15,7 @@ module.exports = Object.freeze({
     CONFIRMATION_MODAL_CLOSE_TYPES : {
         LEAD_DELETE : "LEAD_DELETE",
     },
-    //Base_URL : "https://sautsokreach.xyz",
-    Base_URL : "http://localhost:8080",
+    Base_URL : "https://sautsokreach.xyz",
+   // Base_URL : "http://localhost:8080",
     
 });
