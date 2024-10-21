a145b56357bc5af23d7ffa7e2a7a24a2ecd4c71e
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2024-10-21 16:55:32 +0700

    Update globalConstantUtil.js

--- src/utils/globalConstantUtil.js
+++ src/utils/globalConstantUtil.js
@@ -16,6 +16,6 @@ module.exports = Object.freeze({
         LEAD_DELETE : "LEAD_DELETE",
     },
    // Base_URL : "https://sautsokreach.xyz",
-    Base_URL : "http://localhost:8080",
-    
+    //Base_URL : "http://localhost:8080",
+    Base_URL : "https://westernapi001-df6d24a5195d.herokuapp.com",
 });
