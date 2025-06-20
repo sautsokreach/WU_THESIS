c53c2afbf1d1a2333f56baf06c1d3fa408e72f12
sokreach <sautsokreach@gmail.com>
2025-06-20 14:35:56 +0700

    test

--- src/utils/globalConstantUtil.js
+++ src/utils/globalConstantUtil.js
@@ -17,5 +17,5 @@ module.exports = Object.freeze({
     },
    // Base_URL : "https://sautsokreach.xyz",
     //Base_URL : "http://localhost:8080",
-    Base_URL : "https://universitywestern-d1d4d5232f5f.herokuapp.com/",
+    Base_URL : "https://universitywestern-d1d4d5232f5f.herokuapp.com/"
 });
