ee6d142c36da9e74ce1022f0d1e567fdaa131440
sokreach <sautsokreach@gmail.com>
2025-06-24 15:10:03 +0700

    Test

--- src/utils/globalConstantUtil.js
+++ src/utils/globalConstantUtil.js
@@ -15,7 +15,6 @@ module.exports = Object.freeze({
     CONFIRMATION_MODAL_CLOSE_TYPES : {
         LEAD_DELETE : "LEAD_DELETE",
     },
-   // Base_URL : "https://sautsokreach.xyz",
     //Base_URL : "http://localhost:8080",
     Base_URL : "https://universitywestern-d1d4d5232f5f.herokuapp.com"
 });
